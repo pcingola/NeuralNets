a710eb667cda8e4a50a3523cfdcd39517faba4ea
Pablo Cingolani <pablo.e.cingolani@gmail.com>
2019-03-21 22:11:04 -0400

    Project updated

--- src/python/backprop/xor.py
+++ src/python/backprop/xor.py
@@ -4,11 +4,11 @@ import math
 import numpy as np
 
 
-RAND_WEIGHT_FACTOR = 0.1
+RAND_WEIGHT_FACTOR = 0.5
 
 # Trainig data
-XOR_INPUTS = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-XOR_OUTPUTS = np.array([0, 1, 1, 0])
+XOR_INPUTS = np.array([[-1, -1], [-1, 1], [1, -1], [1, 1]])
+XOR_OUTPUTS = np.array([0.1, 0.9, 0.9, 0.1])
 
 neuron_id = 0
 
@@ -26,26 +26,25 @@ class Neuron:
         self.fan_in = len(input_neurons)
         self.out = 0.0
         self.w = RAND_WEIGHT_FACTOR * (2.0 * np.random.rand((self.fan_in)) - 1.0)
-        self.w = np.array([0.2, -0.1, 0.1])
         self.input_neurons = input_neurons
-        # Error back-propagation
-        self.delta = 0.0
-        self.delta_sum = 0.0
-        self.delta_w = np.zeros((self.fan_in))
+        self.reset_delta_w()
+        self.delta = 0
+        self.delta_sum = 0
 
     def backprop(self):
         ''' Perform a back-propagation '''
         self.delta = self.delta_sum * self.phi_prime()
         for i, n in enumerate(self.input_neurons):
-            n.delta_sum += self.delta * self.w[i]
-            self.delta_w[i] = n.out * self.delta
+            n.delta_sum += self.w[i] * self.delta
+            self.delta_w[i] += n.out * self.delta
 
     def calc(self):
         ''' Calculate neuron's output '''
+        self.delta = 0
+        self.delta_sum = 0
         ins = np.array([n.out for n in self.input_neurons])
         h = np.dot(self.w, ins)
         self.out = self.phi(h)
-        print(f"id{self.id}\tins: {ins}\th: {h}\tout: {self.out}")
 
     def phi(self, h):
         ''' Neuron's transfer function '''
@@ -55,12 +54,15 @@ class Neuron:
         ''' Neuron's transfer function derivate '''
         return self.out * (1.0 - self.out)
 
+    def reset_delta_w(self):
+        self.delta_w = np.zeros((self.fan_in)) # Weight update
+
     def update_w(self, eta):
         ''' Update weights '''
         self.w -= eta * self.delta_w
 
     def __str__(self):
-        return f"id: {self.id}, output: {self.out}, weights: {self.w}, delta: {self.delta}, delta_sum: {self.delta_sum}"
+        return f"id: {self.id}, output: {self.out}, weights: {self.w}, delta: {self.delta}, delta_sum: {self.delta_sum}, delta_w: {self.delta_w}"
 
 
 class Network:
@@ -81,8 +83,6 @@ class Network:
         # Create layers
         self.layers = [[in1, in2], [n1, n2], [self.n_out]]
         self.neurons = [n for l in self.layers[1:] for n in l]
-        self.neurons_rev = self.neurons.copy()
-        reversed(self.neurons_rev)
 
     def backprop(self, num_in):
         ''' Back-propagate weights '''
@@ -91,45 +91,55 @@ class Network:
             n.delta_sum = 0
         # Calculate output delta
         diff_loss = (self.data_out[num_in] - self.n_out.out)
-        self.n_out.delta_sum = diff_loss
+        self.n_out.delta_sum = -diff_loss
         # Backpropagation
-        for n in self.neurons_rev:
+        for n in reversed(self.neurons):
             n.backprop()
-            print(f"\tBP: {n}")
-        print(f"BP: {self.layers[0][0].delta_sum} , {self.layers[0][1].delta_sum} => {self.layers[1][0].delta_sum} , {self.layers[1][1].delta_sum} => {self.n_out.delta_sum}\n")
         return diff_loss * diff_loss
 
     def calc(self):
         ''' Calculate outputs for all neurons (forward propagation) '''
         for n in self.neurons:
             n.calc()
-            print(f"\tFW: {n}")
-        print(f"FW: {self.layers[0][0].out} , {self.layers[0][1].out} => {self.layers[1][0].out} , {self.layers[1][1].out} => {self.n_out.out}")
+
+    def evaluate(self):
+        ''' Evaluate network for each input '''
+        l0 = self.layers[0]
+        num_samples = self.data_in.shape[0]
+        for i in range(num_samples):
+            self.set_inputs(i)
+            self.calc()
+            print(f"{l0[0].out}\t{l0[1].out}\t=>\t{self.n_out.out}")
 
     def set_inputs(self, num_in):
         ''' Set netwok inputs with sample number 'num_in' '''
         for j, n_in in enumerate(self.layers[0]):
             n_in.out = self.data_in[num_in, j]
 
-    def train(self):
+    def train(self, eta):
         ''' Train a neural network (one iteration) '''
         sum_loss = 0.0
         num_samples = self.data_in.shape[0]
         n_out = self.layers[2][0]
-        eta = 0.1
+        for n in self.neurons:
+            n.reset_delta_w()
         # For each input sample...
         for i in range(num_samples):
             self.set_inputs(i)
             self.calc()
             sum_loss += self.backprop(i)
-        loss = sum_loss / (2.0 * num_samples)
-        print(f"loss = {loss}")
         # Update weights
         for n in self.neurons:
             n.update_w(eta)
+        # Return loss
+        loss = sum_loss / (2.0 * num_samples)
+        return loss
 
 
 # Train
 net = Network(XOR_INPUTS, XOR_OUTPUTS)
-for i in range(100):
-    net.train()
+eta = 1
+for i in range(2000):
+    loss = net.train(eta)
+    print(f"Iteration: {i}\tloss: {loss}")
+net.evaluate()
